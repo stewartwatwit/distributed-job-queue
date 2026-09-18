0a3f3264b144d7cd30bf46c4933e8d643bc4bb5c
wstew1055 <wstew1055@gmail.com>
2026-09-17 23:02:33 -0400

    Add priority repository function

--- backend/src/main/java/com/willstewart/jobqueue/job/JobRepository.java
+++ backend/src/main/java/com/willstewart/jobqueue/job/JobRepository.java
@@ -1,6 +1,11 @@
 package com.willstewart.jobqueue.job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional; 
 
 public interface JobRepository extends JpaRepository<Job, String> {
     
+    @Query("SELECT j FROM Job j WHERE j.status = 'PENDING' ORDER BY j.priority DESC, j.createdAt ASC")
+    Optional<Job> findNextPendingJob();
 }
