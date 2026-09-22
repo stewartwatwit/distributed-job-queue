e7f02f85c1bef734f1f2ee53eb10e7c217d0cc62
wstew1055 <wstew1055@gmail.com>
2026-09-22 15:05:15 -0400

    Added job request dto

--- backend/src/test/java/com/willstewart/jobqueue/job/JobRepositoryIntegrationTest.java
+++ backend/src/test/java/com/willstewart/jobqueue/job/JobRepositoryIntegrationTest.java
@@ -23,7 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @Testcontainers
 @SpringBootTest 
